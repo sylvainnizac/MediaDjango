eb94e353eca4209b54c988b858d8ff7a7d9d9f6d
Sylvain Mekarnia-Nizac <sylvain.nizac@aum.bio>
2022-03-01 17:49:32 +0100

    add can_be_sold in product tests

--- tests/tests_products.py
+++ tests/tests_products.py
@@ -11,11 +11,12 @@ class ProductModelTests(TestCase):
         """
         ensure_price_is_positive should results True for products where price is zero or positive
         """
-        prod = Product(name="test",price=1.99, stockpile=10)
+        prod = Product(name="test",price=1.99, stockpile=10, can_be_sold=True)
         prod.save()
         self.assertEqual((prod.price == 1.99), True)
         self.assertEqual((prod.stockpile == 10), True)
         self.assertEqual((prod.name == "test"), True)
+        self.assertTrue(prod.can_be_sold)
 
 
 class ProductsViewsTests(TestCase):
@@ -37,13 +38,13 @@ class ProductsViewsTests(TestCase):
 
     def test_index_view_with_products(self):
         """
-        If no products exist, an appropriate message should be displayed.
+        If products exist, they should be displayed.
         """
         response = self.client.get("/products/")
 
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "new product")
-        self.assertEqual(len(response.context["products"]), 2)
+        self.assertEqual(len(response.context["products"]), 3)
 
     def test_delete_view(self):
         """
@@ -54,30 +55,31 @@ class ProductsViewsTests(TestCase):
         self.assertEqual(response.status_code, 200)
 
         all_products_query = Product.objects.all()
-        self.assertEqual(len(all_products_query), 1)
+        self.assertEqual(len(all_products_query), 2)
 
     def test_create_view(self):
         """
         Should create the product
         """
-        response = self.client.post("/products/create_product", {"name": "created product", "price": 4.44, "stockpile": 33})
+        response = self.client.post("/products/create_product", {"name": "created product", "price": 4.44, "stockpile": 33, "can_be_sold": True})
 
         self.assertEqual(response.status_code, 201)
 
         all_products_query = Product.objects.all()
-        self.assertEqual(len(all_products_query), 3)
+        self.assertEqual(len(all_products_query), 4)
 
     def test_update_view(self):
         """
         Should update the product
         """
-        response = self.client.patch("/products/update_product/1", urlencode({"name": "updated product", "price": 4.44, "stockpile": 33}), content_type="text")
+        response = self.client.patch("/products/update_product/1", urlencode({"name": "updated product", "price": 4.44, "stockpile": 33, "can_be_sold": False}), content_type="text")
 
         self.assertEqual(response.status_code, 200)
 
         all_products_query = Product.objects.all()
-        self.assertEqual(len(all_products_query), 2)
+        self.assertEqual(len(all_products_query), 3)
         product_query = Product.objects.get(id=1)
         self.assertEqual(product_query.name, "updated product")
         self.assertEqual(product_query.price, 4.44)
         self.assertEqual(product_query.stockpile, 33)
+        self.assertEqual(product_query.can_be_sold, False)
